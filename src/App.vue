68a4449da3760c0adeb99bca3c9385c9fd726f3a
noob <1984383225@qq.com>
2019-11-19 13:26:15 +0800

    add del

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <todolist/>
+  </div>
+</template>
+
+<script>
+import todolist from './components/todolist'
+
+export default {
+  name: 'App',
+  components: {
+    todolist
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/todolist.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="text" placeholder="请输入需要添加的姓名" v-model="addName" />
+    <button @click="submitData">add</button>
+    <div v-for="(p,index) in list" :key="index">
+      {{index}} -- {{p.name}} <button @click="deleteName">del</button>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "todolist",
+        data() {
+            return {
+                addName:'',
+                list:[{name: 'fg'},{name:'sfx'}]
+            }
+        },methods: {
+            submitData() {
+                this.list.push({name:this.addName})
+            },
+            deleteName($event) {
+                this.list.splice($event.index,1)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
