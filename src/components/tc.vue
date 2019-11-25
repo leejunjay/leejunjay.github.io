a55781e396b6c37774786f429804bc2d384b4752
李俊杰 <44627299+leejunjay@users.noreply.github.com>
2019-11-25 14:47:54 +0800

    tc c

---
+++ src/components/tc.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <button @click="showTC">弹窗</button>
+    <div :class="{show:isShow}">
+      <div
+        style="display: flex;justify-content: center;align-items: center;width: 100%;height: 100%;position: fixed;top:0;background: rgba(0,0,0,0.5);">
+        <div style="border: 2px solid #ccc;background: white;width: 50vw;height: 50vh;padding: 1%;">
+          <div class="header" style="overflow:hidden;">
+            <div class="header-title">
+            </div>
+            <div class="header-close">
+              <button @click="hideTc"
+                      style="background: none;border: 2px solid #ccc;float: right;padding: 10px;border-radius: 10px;color: red;font-weight: bold;">
+                关闭
+              </button>
+            </div>
+          </div>
+          <div style="height: 50px;margin-top:15px;">
+            <div style="width: 10%;float: left;height: 100%;display: flex;align-items: center;">
+              <label>姓名：</label>
+            </div>
+            <div style="width: 90%;float: left;height: 100%">
+              <input style="width: 100%;height:100%;border-radius: 10px;text-indent: 1em;"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "tc",
+        data() {
+            return {
+                isShow: true
+            }
+        },
+        methods: {
+            showTC() {
+                this.isShow = false
+            },
+            hideTc() {
+                this.isShow = true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+  input:focus, button:focus {
+    outline: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  .show {
+    display: none;
+  }
+</style>
--- src/components/todolist.vue
+++ src/components/todolist.vue
@@ -17,39 +17,15 @@
       </div>
       <br/>
       <br/>
-      <div>
-        <button @click="showTC">弹窗</button>
-        <div :class="{show:isShow}">
-          <div
-            style="display: flex;justify-content: center;align-items: center;width: 100%;height: 100%;position: fixed;top:0;background: rgba(0,0,0,0.5);">
-            <div style="border: 2px solid #ccc;background: white;width: 50vw;height: 50vh;padding: 1%;">
-              <div class="header" style="overflow:hidden;">
-                <div class="header-title">
-                </div>
-                <div class="header-close">
-                  <button @click="hideTc" style="background: none;border: 2px solid #ccc;float: right;padding: 10px;border-radius: 10px;color: red;font-weight: bold;">
-                    关闭
-                  </button>
-                </div>
-              </div>
-              <div style="height: 50px;margin-top:15px;">
-                <div style="width: 10%;float: left;height: 100%;display: flex;align-items: center;">
-                  <label>姓名：</label>
-                </div>
-                <div style="width: 90%;float: left;height: 100%">
-                  <input style="width: 100%;height:100%;border-radius: 10px;text-indent: 1em;"/>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Tc/>
     </div>
   </div>
 </template>
 
 <script>
+    import Tc from './tc'
     export default {
+        components: {Tc},
         data() {
             return {
                 addName: '',
@@ -83,12 +59,6 @@
                     p.defaultOk = true
                     p.disabled = true
                 }
-            },
-            showTC() {
-                this.isShow = false
-            },
-            hideTc() {
-                this.isShow = true
             }
         }, computed: {
             searchf() {
@@ -103,13 +73,5 @@
 <style scoped>
   input:focus,button:focus {
     outline: none;
-  }
-
-  button {
-    cursor: pointer;
-  }
-
-  .show {
-    display: none;
   }
 </style>
